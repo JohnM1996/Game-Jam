cad67d57ef459f52042d077b73de4c89a99e6c0a
GuyMan <eelydoc@gmail.com>
2019-02-23 12:00:49 -0500

    Started working on script. Setup scene

---
+++ GameJam-Intersection/Assets/Scripts/RoomSpawning.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawning : MonoBehaviour 
+{
+	public GameObject RoomToSpawn;
+    public Vector3 offset;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnTriggerEnter(Collider collision)
+	{
+		if(collision.gameObject.tag == "Player")
+		{
+         SpawnRoom();
+		}
+	}
+
+	public void SpawnRoom()
+	{
+     Instantiate(RoomToSpawn, transform.position + offset, transform.rotation);
+	}
+}
